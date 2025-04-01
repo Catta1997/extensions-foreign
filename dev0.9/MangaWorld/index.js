bf2487766a655afd5240ac3c949b4475d9618707
Catta1997 <34333654+Catta1997@users.noreply.github.com>
2025-04-01 07:25:11 +0000

    Deploying to gh-pages from @ Catta1997/extensions-foreign@f66c24ae863199ac6b95b7c8f62081735ef3a368 🚀

--- dev0.9/MangaWorld/index.js
+++ dev0.9/MangaWorld/index.js
@@ -509,24 +509,9 @@ class MangaWorld {
         const request = App.createRequest({
             url: `${this.baseUrl}/manga/${mangaId}`,
             method: 'GET',
-            followRedirect: false
-            // Prevent auto-following redirects
         });
         const response = await this.requestManager.schedule(request, this.RETRIES);
-        let redirectedUrl = response.headers?.location;
-        if (!redirectedUrl) {
-            redirectedUrl = `${this.baseUrl}/manga/${mangaId}`;
-        }
-        const match = redirectedUrl.match(/\/manga\/([^/]+)\/([^/?#]+)/);
-        if (match) {
-            mangaId = match[1];
-        }
-        const newRequest = App.createRequest({
-            url: `${this.baseUrl}/manga/${mangaId}`,
-            method: 'GET'
-        });
-        const newResponse = await this.requestManager.schedule(newRequest, this.RETRIES);
-        const $ = this.cheerio.load(newResponse.data);
+        const $ = this.cheerio.load(response.data);
         return this.parser.parseChapters($, mangaId, this);
     }
     async getChapterDetails(mangaId, chapterId) {
@@ -774,8 +759,7 @@ class Parser {
                 continue;
             pages.push(imageUrl.trim());
         }
-        mangaId = `4012/masamune-kun-no-revenge-anthology-comic----- ${mangaId}----${id}`;
-        id = `67eafb8e3db8c030bf86756a-----${id}`;
+        mangaId = `----- /${mangaId}/----/${id}/----`;
         return App.createChapterDetails({
             id,
             mangaId,
