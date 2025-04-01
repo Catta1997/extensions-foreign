b9c2989ce4429b7f8432871ec7ac42927c6bb24f
Catta1997 <edoardo.cattarin.97@gmail.com>
2025-04-01 09:06:03 +0200

    Update MangaWorld.ts

--- src/MangaWorld/MangaWorld.ts
+++ src/MangaWorld/MangaWorld.ts
@@ -70,11 +70,23 @@ export class MangaWorld implements SearchResultsProviding, MangaProviding, Chapt
             url: `${this.baseUrl}/manga/${mangaId}`,
             method: 'GET',
             followRedirect: false
-        })
+            // Prevent auto-following redirects
+        });
         const response = await this.requestManager.schedule(request, this.RETRIES)
-        //const finalUrl = response.request?.url || `${this.baseUrl}/manga/${mangaId}`
-        mangaId = response.request?.url || `${this.baseUrl}/manga/${mangaId}`
-        const $ = this.cheerio.load(response.data)
+        let redirectedUrl = response.headers?.location;
+        if (!redirectedUrl) {
+            redirectedUrl = `${this.baseUrl}/manga/${mangaId}`
+        }
+        const match = redirectedUrl.match(/\/manga\/([^/]+)\/([^/?#]+)/)
+        if (match) {
+            mangaId = match[1]
+        }
+        const newRequest = App.createRequest({
+            url: `${this.baseUrl}/manga/${mangaId}`,
+            method: 'GET'
+        })
+        const newResponse = await this.requestManager.schedule(newRequest, this.RETRIES)
+        const $ = this.cheerio.load(newResponse.data)
         return this.parser.parseChapters($, mangaId, this)
     }
 
