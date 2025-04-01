1e54177b0b444552332823dbce98a30d149f2c72
Catta1997 <edoardo.cattarin.97@gmail.com>
2025-04-01 11:59:21 +0200

    tag test

--- src/MangaWorld/MangaWorld.ts
+++ src/MangaWorld/MangaWorld.ts
@@ -23,7 +23,7 @@ import { URLBuilder } from './helper'
 const MW_DOMAIN = 'https://www.mangaworld.nz'
 
 export const MangaWorldInfo: SourceInfo = {
-    version: '0.2.4',
+    version: '0.2.5',
     name: 'MangaWorld',
     description: 'Extension that pulls manga from MangaWorld (0.9).',
     author: 'NmN',
