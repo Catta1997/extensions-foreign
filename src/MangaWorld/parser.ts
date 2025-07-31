65d0400d8d57d4bac702fad2ed1e49e5ba67082f
Sinon <34333654+Catta1997@users.noreply.github.com>
2025-07-31 15:51:24 +0200

    Update parser.ts

--- src/MangaWorld/parser.ts
+++ src/MangaWorld/parser.ts
@@ -27,7 +27,7 @@ export class Parser {
                         .find('a')
                         .each((_, e) => {
                             label_arr.push($(e).text()) &
-                                id_arr.push($(e).attr('href')?.replace('https://www.mangaworld.nz/archive?genre=', '') ?? '')
+                                id_arr.push($(e).attr('href')?.replace('https://www.mangaworld.cx/archive?genre=', '') ?? '')
                         })
                     break
                 case 2:
